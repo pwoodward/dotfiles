2da393c769f0a853c691fa603fd0b3c5fb2b566a
Peter Woodward <peter.woodward@gmail.com>
2024-05-04 08:38:48 -0500

    Feat: adding lazyvim config

---
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  -- add symbols-outline
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      window = {
+        position = "right",
+        width = 80,
+      },
+    },
+  },
+}
