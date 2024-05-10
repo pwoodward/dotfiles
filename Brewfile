6ca00926275d488815488f4c239e4b5942c3f1fb
Peter Woodward <peter.g.woodward@ibm.com>
2024-05-10 16:16:53 -0500

    Feat: Migrating config over

---
+++ Brewfile
@@ -0,0 +1,43 @@
+# Install this file by running $ brew bundle
+tap "homebrew/bundle"
+tap "homebrew/cask-fonts"
+tap "homebrew/services"
+tap "koekeishiya/formulae"
+
+# Command line apps
+brew "autojump"
+brew "neovim"
+brew "nvm"
+brew "jq"
+brew "lf"
+brew "gradle"
+brew "gradle@7"
+brew "gradle@6"
+brew "openjdk@11"
+brew "go"
+brew "podman"
+brew "ripgrep"
+brew "tmux"
+brew "yabai"
+brew "skhd"
+brew "zsh"
+brew "zsh-completions"
+brew "zsh-syntax-highlighting"
+
+# Helix LSPs
+brew "helix"
+brew "jdtls"
+brew "gopls"
+brew "dlv"
+brew "marksman"
+brew "bash-language-server"
+brew "yaml-language-server"
+brew "docker-ls"
+
+
+# Gui apps
+cask "alacritty"
+cask "wezterm"
+cask "firefox"
+cask "font-fira-code-nerd-font"
+#cask "google-chrome"
---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+config.color_scheme = "Tokyo Night"
+
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Regular" })
+config.font_size = 15.0
+
+config.enable_kitty_keyboard = true
+
+-- and finally, return the configuration to wezterm
+return config
