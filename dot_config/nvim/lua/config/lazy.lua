4ff59bddb5a73355886e9c24680d9e8871a638e2
Peter Woodward <peter.g.woodward@ibm.com>
2025-12-08 14:50:01 -0600

    fix: correcting paths and duplicating variables

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -17,11 +17,11 @@ require("lazy").setup {
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
+    -- { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.json" },
+    --{ import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
--- dot_config/zsh/secrets.zsh
+++ dot_config/zsh/secrets.zsh
@@ -1,2 +1,5 @@
 export ARTIFACTORY_USERNAME="op://Employee/Artifactory/username"
 export ARTIFACTORY_PASSWORD="op://Employee/Artifactory/credential"
+
+export ARTIFACTORY_USER=$ARTIFACTORY_USERNAME
+export ARTIFACTORY_TOKEN=$ARTIFACTORY_PASSWORD
