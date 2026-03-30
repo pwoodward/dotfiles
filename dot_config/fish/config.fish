89e8f7bc1c2b4d284f2dbac242ad33b189f9928b
Peter Woodward <peter.g.woodward@ibm.com>
2026-03-30 14:43:36 -0500

    feat: adding television and minor zellij updates

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     atuin init fish | source
+    tv init fish | source
     starship init fish | source
 end
