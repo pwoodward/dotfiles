54db238b9ab315b1066bcf2e9f34ae86813d4fdb
Peter Woodward <peter.g.woodward@ibm.com>
2026-08-25 11:34:56 -0500

    fix: Updating secrets and minor config changes

--- dot_config/zsh/secrets.zsh
+++ dot_config/zsh/secrets.zsh
@@ -4,3 +4,5 @@ export IBMCLOUD_TOKEN="op://Employee/IBM Cloud/credential"
 
 export ARTIFACTORY_USER=$ARTIFACTORY_USERNAME
 export ARTIFACTORY_TOKEN=$ARTIFACTORY_PASSWORD
+
+export BOB_API_KEY="op://Employee/Bob API Key/credential"
